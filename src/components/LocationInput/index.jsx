4f849ba97a888522f77cb218fb96e8dc6f91ff6e
Henry Tottle <henrywilliamtottle@gmail.com>
2024-10-15 15:26:40 +0100

    mapped stations

--- src/components/LocationInput/index.jsx
+++ src/components/LocationInput/index.jsx
@@ -1,17 +1,14 @@
 function LocationInput({stations}) {
+    console.log(stations)
+    stations.stations.map((station) => {
+        console.log(station)
+    })
     return (
        <div>
            <select className='font-Anton italic bg-amber-100 rounded-3xl p-3'>
-               {console.log(stations.stations)}
-               {/*{stations.map((station, i) => (*/}
-               {/*    <option key={i}>{station.code}</option>*/}
-               {/*))}*/}
-               <option className='font-Anton italic'>Station 1</option>
-               <option>Station 2</option>
-               <option>Station 3</option>
-               <option>Station 4</option>
-               <option>Station 5</option>
-               <option>Station 6</option>
+               {stations.stations.map((station, i) =>
+                   (<option key={i}>{station.code}, {station.name}</option>)
+               )}
            </select>
 
        </div>
