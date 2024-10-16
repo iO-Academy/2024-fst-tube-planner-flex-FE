e4a6618e61b336387031a5573a2af1a78ff68ac6
Henry Tottle <henrywilliamtottle@gmail.com>
2024-10-16 19:59:38 +0100

    ready to merge to Story2 branch

--- src/components/PlanRouteButton/index.jsx
+++ src/components/PlanRouteButton/index.jsx
@@ -10,7 +10,7 @@ function PlanRouteButton({boolean, setBoolean}) {
     return(
         <>
             {/*add event listener to button*/}
-            <button onClick={clickHandler} className='border-2 border-red-700 rounded-3xl p-2 active:bg-amber-50'>Plan Route</button>
+            <button onClick={clickHandler} className='font-Anton italic text-2xl border-2 border-red-700 rounded-3xl p-2 active:bg-amber-50'>Plan Route</button>
         </>
     )
 }
