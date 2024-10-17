b4379b1ff2e2b68785328333fd88e3348464353b
Henry Tottle <henrywilliamtottle@gmail.com>
2024-10-17 14:23:05 +0100

    added some accessibility

--- src/components/RouteCard/index.jsx
+++ src/components/RouteCard/index.jsx
@@ -18,7 +18,9 @@ function RouteCard({journeyInfo}) {
     return (
         journeyInfo && journeyInfo.length > 0 &&
 
-        <div className='container grid-cols-1 flex justify-evenly gap-3 border-2 p-5 rounded-2xl shadow'>
+        <div className='container grid-cols-1 flex justify-evenly gap-3 border-2 p-5 rounded-2xl shadow'
+        role='region' aria-labelledby='journey-info'>
+            <h2 id={'journey-info'} className='font-Anton'>Journey Information</h2>
             <div className='font-Anton'>
                 <div>
                     <h2>From: {journeyInfo[0].from}</h2>
@@ -28,8 +30,8 @@ function RouteCard({journeyInfo}) {
                 <h2>Time: {timeConversion(journeyInfo[0].time)}</h2>
             </div>
             <div className='font-Anton'>
-                <h2>Stops: {journeyInfo[0].stations.length} - <button className='border-dotted border-2 border-red-300 rounded' onClick={clickHandler}>{showMore === false ? 'show more' : 'show less'}</button></h2>
-                {showMore && (<ul className='text-sm'>
+                <h2>Stops: {journeyInfo[0].stations.length} - <button aria-expanded='showMore' aria-controls='station-list' className='border-dotted border-2 border-red-300 rounded' onClick={clickHandler}>{showMore === false ? 'show more' : 'show less'}</button></h2>
+                {showMore && (<ul className='text-sm' id='station-list' aria-label='List of stations'>
                     {(journeyInfo[0].stations).map((station, i) => {
                         return (
                             <li className='list-disc' key={i}>{station[0] + ' - ' + timeConversion(station[1])}</li>)
